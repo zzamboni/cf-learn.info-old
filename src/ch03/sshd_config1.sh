3865d75ee612ef842b6c1cf1cc0880871ffd5df8
Diego Zamboni <diego@zzamboni.org>
2012-01-08 16:37:20 -0600

    Removed src/, will be moved to master branch

--- src/ch03/sshd_config1.sh
+++
@@ -1,1 +0,0 @@
-echo "AllowRoot no" >> /etc/ssh/sshd_config
--- src/ch03/sshd_config2.sh
+++
@@ -1,3 +0,0 @@
-(grep -iq AllowRoot /etc/ssh/sshd_config ||
- echo "AllowRoot no" >> /etc/ssh/sshd_config) &&
- sed -i 's/^.*AllowRoot .*$/AllowRoot no/;' /etc/ssh/sshd_config
